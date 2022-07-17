04ee7dcc2d2dbfe9e0883162507525e92edbe8e3
Nicholas White <nickobelic1999@gmail.com>
2022-07-17 22:47:33 +1000

    Changed: libs directory to extern_libs directory, is more clearly labels this directory and its purpose.

---
+++ toolbox/credentials.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nickobelic1999 on 7/17/22.
+//
+
+#include "credentials.hpp"
---
+++ toolbox/credentials.hpp
@@ -0,0 +1,10 @@
+//
+// Created by nickobelic1999 on 7/17/22.
+//
+
+#pragma once
+
+class credentials {
+
+};
+
