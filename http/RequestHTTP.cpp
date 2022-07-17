19e70a287936e9c662cebca6b35840a7cf4e8ca0
Nicholas White <nickobelic1999@gmail.com>
2022-07-17 22:33:22 +1000

    Added: RequestHTTP class to project, this will be renamed to something more appropriate in a later commit. The toolbox was added to this commit this will be where helper functions go and where small project addtions get added to the project. This commit is still a work in progress

---
+++ http/RequestHTTP.cpp
@@ -0,0 +1,8 @@
+//
+// Created by nickobelic1999 on 7/17/22.
+//
+
+#include "RequestHTTP.hpp"
+
+namespace MissionControl_HTTP {
+} // MissionControl_HTTP
---
+++ http/RequestHTTP.hpp
@@ -0,0 +1,16 @@
+//
+// Created by nickobelic1999 on 7/17/22.
+//
+
+#ifndef MISSIONCONTROL_REQUESTHTTP_HPP
+#define MISSIONCONTROL_REQUESTHTTP_HPP
+
+namespace MissionControl_HTTP {
+
+    class RequestHTTP {
+
+    };
+
+} // MissionControl_HTTP
+
+#endif //MISSIONCONTROL_REQUESTHTTP_HPP
